84adfef841ec09f324ac0e700bfec9d91f737915
Ahmed Olalekan <olalekanahmed6@gmail.com>
2024-04-16 13:22:52 +0100

    ADD SOLUTIONS

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,18 +5,21 @@
 from flask import request
 from typing import List, TypeVar
 
+
 class Auth():
-    """ Class that defines the Basic 
+    """ Class that defines the Basic
         authentication methods
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ require authentication to resources
         """
         return False
+
     def authorization_header(self, request=None) -> str:
         """ The authorization header
         """
         return None
+
     def current_user(self, request=None) -> TypeVar('User'):
         """ The User to be authenticate upon request
         """
